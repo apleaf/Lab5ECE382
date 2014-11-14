278a4def9667a175e6809e291188ca66ce3d3e47
apleaf <c16alexander.leaf@usafa.edu>
2014-11-13 18:18:57 -0700

    added required .h file

---
+++ start.h
@@ -0,0 +1,57 @@
+//-----------------------------------------------------------------
+// Name:	Coulston
+// File:	lab5.h
+// Date:	Fall 2014
+// Purp:	Include file for the MSP430
+//-----------------------------------------------------------------
+
+//-----------------------------------------------------------------
+// Page 76 : MSP430 Optimizing C/C++ Compiler v 4.3 User's Guide
+//-----------------------------------------------------------------
+typedef		unsigned char		int8;
+typedef		unsigned short		int16;
+typedef		unsigned long		int32;
+typedef		unsigned long long	int64;
+
+#define		TRUE				1
+#define		FALSE				0
+
+//-----------------------------------------------------------------
+// Function prototypes found in lab5.c
+//-----------------------------------------------------------------
+void initMSP430();
+__interrupt void pinChange (void);
+__interrupt void timerOverflow (void);
+
+
+//-----------------------------------------------------------------
+// Each PxIES bit selects the interrupt edge for the corresponding I/O pin.
+//	Bit = 0: The PxIFGx flag is set with a low-to-high transition
+//	Bit = 1: The PxIFGx flag is set with a high-to-low transition
+//-----------------------------------------------------------------
+
+#define		IR_PIN			(P2IN & BIT6)
+#define		HIGH_2_LOW		P2IES |= BIT6
+#define		LOW_2_HIGH		P2IES &= ~BIT6
+
+
+#define		averageLogic0Pulse	551
+#define		averageLogic1Pulse	1617
+#define		averageStartPulse	4397
+#define		minLogic0Pulse		averageLogic0Pulse - 200
+#define		maxLogic0Pulse		averageLogic0Pulse + 200
+#define		minLogic1Pulse		averageLogic1Pulse - 200
+#define		maxLogic1Pulse		averageLogic1Pulse + 200
+#define		minStartPulse		averageStartPulse - 200
+#define		maxStartPulse		averageStartPulse + 200
+
+#define		PWR		0x61A0F00E
+#define		ONE		0x61A000FE
+#define		TWO		0x61A0807E
+#define		THR		0x61A040BE
+#define     ZER     0x61A0906E
+
+#define		VOL_UP	0x61A030CE
+#define		VOL_DW	0x61A0B04E
+#define		CH_UP	0x61A050AE
+#define		CH_DW	0x61A0D02E
